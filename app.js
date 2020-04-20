051ffac6e067b2958f35350dc97ad0cc91d5abd9
kaeteyaruyo <kaeteyaruyo@gmail.com>
2020-04-20 15:10:58 +0800

    Introduce pug
    * Introduce pug as template engine
        * Change existing html code into pug
        * Add view engine in backend so it can just render .pug files directly
    * Modify some style in signin / signup page

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const port = 8888;
 const root = process.cwd();
 const db = {};
 
+app.set('view engine', 'pug');
+app.set('views', path.join(root, '/static/pug'));
 app.use(express.static(path.join(root, '/static')));
 app.use(parser.urlencoded( { extended: true } ));
 app.use(parser.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(root, 'static/html/index.html'));
+    res.render('index');
 });
 
 app.get('/signup', (req, res) => {
-    res.sendFile(path.join(root, 'static/html/signup.html'));
+    res.render('signup');
 })
 
 app.post('/signup', (req, res) => {
@@ -24,18 +26,20 @@ app.post('/signup', (req, res) => {
 })
 
 app.get('/signin', (req, res) => {
-    res.sendFile(path.join(root, 'static/html/signin.html'));
+    res.render('signin');
 })
 
 app.post('/signin', (req, res) => {
     if(db[req.body.account] && db[req.body.account] === req.body.password){
         res.redirect(`/user/${ req.body.account }`);
     }
-    res.redirect('signin');
+    else {
+        res.redirect('signin');
+    }
 })
 
 app.get('/user/:account', (req, res) => {
-    res.sendFile(path.join(root, 'static/html/user.html'));
+    res.render('user');
 });
 
 app.listen(port, () => {

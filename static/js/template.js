8523d191ca47f7eb1bf21eab097b3064fafc540a
ArielWu0203 <lucky020372@gmail.com>
2020-06-17 21:16:30 +0800

    add imageEditor in template.js

--- static/js/template.js
+++ static/js/template.js
@@ -1,5 +1,11 @@
 const ImageEditor = require('tui-image-editor');
 
 const imgUrl = '/img/template/blank.jpg';
-let imageEditor = new ImageEditor(document.getElementById('tui-image-editor'));
-imageEditor.usageStatistics(false);
+let imageEditor = new ImageEditor(document.getElementById('tui-image-editor'), {
+    cssMaxWidth: 700,
+    cssMaxHeight: 500,
+    selectionStyle: {
+        cornerSize: 20,
+        rotatingPointOffset: 70
+    }
+});

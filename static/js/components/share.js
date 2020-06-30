2cefd7382c33697514d17334f29d869baea95b61
kaeteyaruyo <kaeteyaruyo@gmail.com>
2020-06-30 20:48:13 +0800

    Add share to socail medias feature
    * Add share to social medias feature. Include:
        * Line
        * Facebook
        * Twitter
        * Plurk

--- static/js/components/share.js
+++ static/js/components/share.js
@@ -1,5 +1,9 @@
 const shareDialog = document.querySelector('.main__share');
 const shareURL = document.querySelector('#share--url');
+const lineURL = document.querySelector('.main__share--snsLine');
+const facebookURL = document.querySelector('.main__share--snsFacebook');
+const twitterURL = document.querySelector('.main__share--snsTwitter');
+const plurkURL = document.querySelector('.main__share--snsPlurk');
 
 document.querySelector('.main__share--close').addEventListener('click', function(){
     shareDialog.style.display = 'none';
@@ -13,5 +17,9 @@ document.querySelector('.main__share--linkButton').addEventListener('click', () 
 
 function openShareDialog(imageId){
     shareURL.value = `${ window.location.origin }/image/${ imageId }`;
+    lineURL.href = `https://social-plugins.line.me/lineit/share?url=${ shareURL.value }`;
+    facebookURL.href = `https://www.facebook.com/dialog/share?app_id=684988175228092&href=${ shareURL.value }`;
+    twitterURL.href = `https://twitter.com/intent/tweet?text=我在Co meme上發現了一張有趣的圖片，快來一起看看吧！\n${ shareURL.value }`
+    plurkURL.href =`http://www.plurk.com/?qualifier=shares&status=我在Co meme上發現了一張有趣的圖片，快來一起看看吧！%0D%0A${ window.location.href }`;
     shareDialog.style.display = 'grid';
 }
